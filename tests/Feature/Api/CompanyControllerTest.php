2f70766ebdc99e05f266af889883749a0b71bab3
daverydan <danny.w.avery@gmail.com>
2024-05-27 12:09:43 -0400

    Fix company index test

--- tests/Feature/Api/CompanyControllerTest.php
+++ tests/Feature/Api/CompanyControllerTest.php
@@ -5,11 +5,11 @@ use App\Models\Company;
 use function Pest\Laravel\{actingAs, getJson};
 
 test('companies index', function () {
-    $response = getJson(route('companies.index', $companies = [
-        'data' => Company::factory()->times(3)->create()->toArray(),
-    ]));
+    $companies = Company::factory()->times(3)->create();
+
+    $response = getJson(route('companies.index'));
 
     $response
         ->assertOk()
-        ->assertJson($companies);
+        ->assertJson(['data' => $companies->toArray()]);
 })->group('companies');
