83716581db1f00953e3ae2ab75eed257364c0772
daverydan <danny.w.avery@gmail.com>
2024-05-27 11:58:30 -0400

    Add Company resources

--- app/Http/Controllers/Api/CompanyController.php
+++ app/Http/Controllers/Api/CompanyController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CompanyCollection;
 use App\Models\Company;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        return response()->json(['data' => Company::all()]);
+        return new CompanyCollection(Company::paginate());
     }
 
     /**
---
+++ app/Http/Resources/CompanyResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CompanyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'website' => $this->website,
+            'logo' => $this->logo,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
